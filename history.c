f9bf38e32f5c52eca0932ea245fff3fc87a099a0
Master Chief <extra07@online.de>
2011-08-04 12:35:01 +0200

    now it test if the icon are alredy downloaded

--- history.c
+++ history.c
@@ -1,17 +1,49 @@
-void save_to_history(char *uri);
+void save_to_history(Client *c);
+Bool existFile(const char *fname);
 
-void save_to_history(char *uri)
+
+void save_to_history(Client *c)
 {
-  char curdate[25];
+  const gchar *icon  = webkit_web_view_get_icon_uri(c->view);
+  const gchar *title = webkit_web_view_get_title(c->view);
+  if(*icon != NULL){
+    WebKitNetworkRequest *request = webkit_network_request_new(icon);
+    WebKitDownload *download = webkit_download_new(request);
+
+    char filename[500];
+    sprintf(filename,"/home/chief/.surf/.history/.icons/%s.ico",(char*)title);
+
+    if(!existFile(filename)){
+      const gchar *desturl = g_filename_to_uri(filename, NULL, NULL);
+      webkit_download_set_destination_uri(download, desturl);
+      webkit_download_start(download);
+    }else printf("\nFile exist\n");
+  }
+
+  char curdate[25], *uri = geturi(c);
   time_t t = time(NULL);
   struct tm *ts = localtime(&t);
 
-  strftime(curdate, 80, "%d:%m:%Y:%H:%M:%S", ts);
+  strftime(curdate, 80, "%d:%m:%Y:%H:%M:%S::", ts);
 
   FILE *f;
   f = fopen(historyfile, "a+");
   fprintf(f, curdate);
+  fprintf(f, (char*)title);
+  fprintf(f, "::");
   fprintf(f, uri);
   fprintf(f, "\n");
   fclose(f);
 }
+
+Bool existFile(const char *fname)
+{
+  FILE * f= fopen (fname,"r");
+    if (!f) return False;
+    else {
+      fclose(f); // dann ging's. Also wieder zu damit
+      return True;
+    } 
+}
+
+
--- surf.c
+++ surf.c
@@ -382,29 +382,7 @@ loadstatuschange(WebKitWebView *view, GParamSpec *pspec, Client *c) {
 		c->progress = 0;
 		update(c);
     if(searched) searched = False;
-    else save_to_history(geturi(c));
-    const gchar *icon  = webkit_web_view_get_icon_uri(c->view);
-    const gchar *title = webkit_web_view_get_title(c->view);
-    printf("\n\ntitle: %s\n\n", (char*)title);
-    if(*icon != NULL){
-      printf("icon: %s\n",(char*)icon);
-      WebKitNetworkRequest *request = webkit_network_request_new(icon);
-      printf("\n\n network request uri: %s\n", (char*)webkit_network_request_get_uri(request));
-      WebKitDownload *download = webkit_download_new(request);
-
-      char filename[500];
-      sprintf(filename,"/home/chief/.surf/.history/.icons/%s.ico",(char*)title);
-
-
-      const gchar *desturl = g_filename_to_uri(filename, NULL, NULL);
-      webkit_download_set_destination_uri(download, desturl);
-      printf("\n download url: %s\n", webkit_download_get_uri(download));
-      printf("\n destination url: %s\n",  webkit_download_get_destination_uri(download));
-      webkit_download_start(download);
-      
-//      const gchar *name = webkit_download_get_suggested_filename(download);
-//      printf("\nname: %s\n", (char*)name);
-    }
+    else save_to_history(c);
 		break;
 	default:
 		break;
