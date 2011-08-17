692da11a3d85c9c439bc713325b86194c923e2e5
Master Chief <extra07@online.de>
2011-08-17 20:50:49 +0200

    added short download script

---
+++ download.rb
@@ -0,0 +1,4 @@
+puts ARGV.inspect
+dmenu = `echo ~/Downloads/#{ARGV[0].split("/").last} | dmenu -fn "-artwiz-cureextra-medium-r-normal--11-110-75-75-p-90-iso8859-1" -sb "#000000" -nb "#000000" -nf "#ffffff" -sf "#00aaff"`.chop
+puts `wget --load-cookies ~/.surf/cookies.txt -O #{dmenu} #{ARGV.first}`
+sleep 10
