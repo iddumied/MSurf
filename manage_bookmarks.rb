d0db2e86b2367bbe64d1f685bdd71373a70c98ef
Master Chief <extra07@online.de>
2011-08-20 16:25:21 +0200

    copleded save

--- manage_bookmarks.rb
+++ manage_bookmarks.rb
@@ -15,7 +15,7 @@ class Bookmark
     Dir.mkdir("#{@home}/.surf/.bookmark") unless Dir.exist?("#{@home}/.surf/.bookmark")
     
     @curdate = { :year => Time.now.year, :month => Time.now.month, :day => Time.now.day, :hour => Time.now.hour, :minute => Time.now.min, :second => Time.now.sec } 
-    @historyb, @historyf = [],[]
+    @historyb, @historyf, @saved = [],[],false
   end
   
   def parse
@@ -29,9 +29,8 @@ class Bookmark
       groub = line.split("::")
       groub.delete_at(0)
       groub.delete_at(groub.length-1)
-      title = groub.last
-      groub.delete_at(groub.length-1)
-      groub = groub.join
+      title = line.sub("::","CATONTHISPOSITION").sub("::","CATONTHISPOSITION").reverse.sub("::","CATONTHISPOSITION".reverse).reverse.split("CATONTHISPOSITION")[2]
+      groub = groub.first
 
       @bookmark << { :date => date, :url => line.split("::").last, :title => title, :groub => groub }
     end
@@ -54,12 +53,23 @@ class Bookmark
   end
 
   def list(pgroub = true, url = false)
+    if  pgroub == true
+    
+    elsif pgroub < 0 or pgroub >= @groubs.length
+      puts "There is no Groub #{pgroub}"    
+      return false
+    
+    end
+
+
+
+
     length = Curses.init_screen.maxx
     Curses.close_screen
 
     @bookmark.each do |groub,elemnts|
-      if pgroub == true or pgroub == groub
-        i = @groubs.find_index(groub)
+      i = @groubs.find_index(groub)
+      if pgroub == true or pgroub == i 
         puts "(#{i}) #{groub}"
         elemnts.each_with_index do |e,i| 
           output = "  #{i}: #{e[:title]}#{ url ? "  -  #{e[:url]}" : ""}"
@@ -77,6 +87,23 @@ class Bookmark
 
   # parameter surce groub, surce element, targte groub
   def shift sg, se, tg
+    if sg < 0 or sg >= @groubs.length 
+      puts "There is no Groub #{sg}"    
+      return false
+    
+    elsif tg < 0 or tg >= @groubs.length
+      puts "There is no Groub #{tg}"    
+      return false
+    
+    elsif @bookmark[@groubs[sg]][se].nil?
+      puts "Groub #{@groubs[sg]} has no entrie #{se}"
+      return false
+
+    elsif tg == sg
+      puts "Warning target groub == source groub"
+    end
+
+
     @historyf = []
     output = "#{@groubs[sg]}: #{@bookmark[@groubs[sg]][se][:title]} => #{@groubs[tg]}"
 
@@ -85,6 +112,7 @@ class Bookmark
     @bookmark[@groubs[tg]] << @bookmark[@groubs[sg]].delete_at(se)
     
     puts output
+    @saved = false 
   end
 
   def clone bookmark = @bookmark
@@ -111,6 +139,7 @@ class Bookmark
     @historyf << { @historyb.last.keys.first => clone }
     puts "undo :: #{@historyb.last.keys.first}"    
     @bookmark = clone( @historyb.delete_at(@historyb.length-1).values.first )
+    @saved = false
   end
   
   def step_for
@@ -122,6 +151,89 @@ class Bookmark
     @historyb << { @historyf.last.keys.first => clone }
     puts "redo :: #{@historyf.last.keys.first}"    
     @bookmark = clone( @historyf.delete_at(@historyf.length-1).values.first )
+    @saved = false
+  end
+
+  def del tg, te = -1
+    if tg < 0 or tg >= @groubs.length
+      puts "There is no Groub #{tg}"    
+      return false
+    
+    elsif te == -1
+       puts "Warning deleting groub"
+
+    elsif @bookmark[@groubs[tg]][te].nil?
+      puts "Groub #{@groubs[tg]} has no entrie #{se}"
+      return false
+
+    end
+
+    @historyf = []
+    if te == -1
+      output = "delete => #{@groubs[tg]}"
+
+      @historyb << { output => clone }
+      @bookmark.delete(@groubs[tg])
+
+    else
+      output = "delete => #{@groubs[tg]}: #{@bookmark[@groubs[tg]][te][:title]}"
+
+      @historyb << { output => clone }
+      @bookmark[@groubs[tg]].delete_at(te)
+    end
+    
+    puts output
+    @saved = false
+  end
+
+  def save
+    number = 0
+    Dir.entries("#{`echo $HOME`.chop}/.surf/.bookmark/").each do |e|
+      if e.include?("bookmark.txt.old.")
+        tmp = e.split(".").last.to_i
+        number = tmp if tmp > number
+      end
+    end
+    number += 1
+    
+    system "cp #{`echo $HOME`.chop}/.surf/bookmark.txt #{`echo $HOME`.chop}/.surf/.bookmark/bookmark.txt.old.#{number}"
+    puts "backup bookmark.txt => bookmark.txt.old.#{number}"
+
+    ary = []
+    @bookmark.each do |k,v|
+      v.each do |entry|
+        ary << [ entry[:date][:second] + entry[:date][:minute]*60 +
+                  entry[:date][:hour]*60*60 + entry[:date][:day]*60*60*24 +
+                  entry[:date][:month]*60*60*24*31 + entry[:date][:year]*60*60*24*31*12, entry ]
+      end
+    end
+
+    
+    file = File.open("#{`echo $HOME`.chop}/.surf/bookmark.txt", "w")
+    until ary.empty?
+      entry = del_min( ary )
+
+      date = sprintf("%02d:%02d:%d:%02d:%02d:%02d",entry[:date][:day], entry[:date][:month],
+                            entry[:date][:year], entry[:date][:hour], entry[:date][:minute],
+                            entry[:date][:second] ) 
+
+      file.puts "#{date}::#{entry[:groub]}::#{entry[:title]}::#{entry[:url]}"
+    end
+    file.close
+    puts "saved #{@historyb.length} changes to bookmark.txt"
+  end
+
+  def del_min( ary )
+    max = ary.first
+    index = 0
+    ary.each_with_index do |e,i| 
+      if e.first < max.first 
+        max = e
+        index = i
+      end
+    end
+
+    return ary.delete_at(index).last
   end
 
 end
@@ -160,10 +272,10 @@ if __FILE__ == $0
        if input.empty?
          bookmark.list true, url
        else
-         bookmark.list input.join, url
+         bookmark.list input.join.to_i, url
        end
       
-    elsif input.split(" ").length == 3
+    elsif input.split(" ").length == 4 and ["s", "shift", "m", "move"].include?( input.split(" ").first )
       input = input.split(" ").map{ |e| e.to_i }
       bookmark.shift(input[0], input[1], input[2])
 
@@ -176,12 +288,28 @@ if __FILE__ == $0
     elsif ["h", "help"].include? input
       puts "ls <opt> <groub>\t\tlist groubs and etries"
       puts "  --url --all -u -a\t\t+url"
-      puts "  <groub>\t\t\tlist onley similar groub"
+      puts "  <groub>\t\t\tlist onley similar groub\n\n"
       puts "q\tquit\t\t\texit programm"
       puts "u\tundo\t\t\tundo last change"
       puts "r\tredo\t\t\tredo last undone change"
+      puts "d\tdel\tdelete\t\tdelete groub or entrie"
+      puts "  d <groub> <entrie>\n\n"
+      puts "s\tsave\t\t\tsave changes to bookmark.txt"
+      
+    elsif ["d","del","delete"].include?( input.split(" ").first ) and [2,3].include?( input.split(" ").length )
+      input = input.split(" ")
+      input.delete_at(0)
+      input.map!{ |e| e.to_i }
+
+      if input.length == 1
+        bookmark.del input.first
+      else
+        bookmark.del input.first, input.last
+      end
+
+    elsif ["s","save"].include? input
+      bookmark.save
       
     end
   end
-
 end
